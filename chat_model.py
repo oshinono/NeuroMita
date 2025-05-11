c2b6bf3760b8736b9cc7e5f4898d7dafd54704bb
VinerX <60321176+VinerX@users.noreply.github.com>
2025-05-11 17:54:26 +0300

    1

--- chat_model.py
+++ chat_model.py
@@ -702,7 +702,7 @@ class ChatModel:
         model_name = model_name.lower()
         if 'gpt' in model_name:
             return 'openai'
-        elif 'gemini' or "gemma" in model_name:
+        elif 'gemini' in model_name or "gemma" in model_name:
             return 'gemini'
         elif 'claude' in model_name:
             return 'anthropic'
